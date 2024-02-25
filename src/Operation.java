760e36ff2aa8532abb9f5b73fa3995a597ed5e34
DDunDDang <thdwns0331@gmail.com>
2024-02-25 15:03:15 +0900

    Prototype

--- src/Operation.java
+++ src/Operation.java
@@ -1,7 +1,3 @@
-import classes.Book;
-import classes.CheckOut;
-import classes.User;
-
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
@@ -297,5 +293,110 @@ public class Operation extends JFrame {
                     checkOut.setEndDate(endDate);
                     checkOut.setCheckOutStatus(CheckOut.CheckOutStatus.END);
                 });
+    }
+}
+
+class Book {
+    private String id;
+    private String name;
+
+    public Book(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
+
+class CheckOut {
+    private LocalDateTime startDate;
+    private LocalDateTime dueDate;
+    private LocalDateTime endDate;
+    private int lateDate;
+    private int baseMonth;
+    private CheckOutStatus checkOutStatus = CheckOutStatus.NEW;
+    private Management management;
+
+    public CheckOut(LocalDateTime startDate, LocalDateTime dueDate, int lateDate, int baseMonth) {
+        this.startDate = startDate;
+        this.dueDate = dueDate;
+        this.lateDate = lateDate;
+        this.baseMonth = baseMonth;
+    }
+
+    public enum CheckOutStatus {
+        // 신규, 장기, 관리중단
+        NEW("신규"),
+        LONG_TERM("장기"),
+        END("관리 중단");
+
+        private String status;
+
+        CheckOutStatus(String status) {
+            this.status = status;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+    }
+
+    public enum Management {
+        // 문자, 전화, 기타
+        MASSAGE("문자"),
+        CALL("전화"),
+        ETC("기타");
+        private String means;
+
+        Management(String means) {
+            this.means = means;
+        }
+
+        public String getMeans() {
+            return means;
+        }
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setBaseMonth(int baseMonth) {
+        this.baseMonth = baseMonth;
+    }
+
+    public CheckOutStatus getCheckOutStatus() {
+        return checkOutStatus;
+    }
+
+    public void setCheckOutStatus(CheckOutStatus checkOutStatus) {
+        this.checkOutStatus = checkOutStatus;
+    }
+
+    public void setManagement(Management management) {
+        this.management = management;
+    }
+}
+
+class User {
+    private String id;
+    private String name;
+    private String phoneNumber;
+    private HashMap<Book, CheckOut> checkOutHashMap = new HashMap<>();
+
+    public User(String id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public HashMap<Book, CheckOut> getCheckOutHashMap() {
+        return checkOutHashMap;
+    }
+
+    public void insertCheckOutInfo(Book book, CheckOut checkOut) {
+        this.checkOutHashMap.put(book, checkOut);
     }
 }
